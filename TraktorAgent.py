73db1ecebabfbf706ddccac3d85801555bef2daa
Simon Stockhause <simon.stockhause@igd.fraunhofer.de~>
2020-01-20 17:30:41 +0100

    updated env var for server ip and port

--- TraktorAgent.py
+++ TraktorAgent.py
@@ -1,20 +1,21 @@
 
 import socket
-
+import os
 
 UDP_IP = os.environ['AGENT_IP']
-UDP_PORT = os.environ['AGENT_PORT']
+UDP_PORT = int(os.environ['AGENT_PORT'])
 class TraktorServer:
     def run(name):
         sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
         sock.bind((UDP_IP, UDP_PORT))
-        print("Server started on: ", UDP_IP, ":",UDP_PORT)
         while True:
             data, addr = sock.recvfrom(1024)
             print("recieved message: ", data)
             print("from: ", addr)
 
 def main():
+    print("Server starting on: ", UDP_IP, ":",UDP_PORT)
+
     traktorServer = TraktorServer()
     traktorServer.run()
 
